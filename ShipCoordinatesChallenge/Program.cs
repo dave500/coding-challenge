3147fd1571830c8525062b32fc38d5ebf22eb036
Dave Hinton <dave.hinton@qnrl.com>
2019-07-10 22:22:36 +0100

    Input validation, Add struct :-) for current state of play

--- ShipCoordinatesChallenge/Program.cs
+++ ShipCoordinatesChallenge/Program.cs
@@ -17,14 +17,21 @@ namespace ShipCoordinatesChallenge
                 throw new InvalidOperationException("Expected 6 arguments like:\n5 3 1 1 E RFRFRF");
             }
 
-            var gridSize = (ia[0], ia[1]);
-            var startPos = (ia[2], ia[3]);
-            var startOrientation = ia[4][0];
+            var gridSize = validateGridSize(ia[0], ia[1]);
+            var startPos = validateStartPos(ia[2], ia[3]);
+            var startOrientation = ia[4];
             var journey = ia[5];
 
-           Console.WriteLine($"gridSize: {gridSize}, startPos: {startPos}, startOrientation: {startOrientation}, journey: {journey}");
+            Console.WriteLine($"gridSize: {gridSize}, startPos: {startPos}, startOrientation: {startOrientation}, journey: {journey}");
 
+            var current = new Current
+            {
+                Orientation = startOrientation,
+                Postion = startPos,
+                GridSize = gridSize
+            };
 
+            Console.ReadLine();
         }
 
        
@@ -37,12 +44,36 @@ namespace ShipCoordinatesChallenge
 
         }
 
-
         private static readonly IDictionary<char, int> movements = new Dictionary<char, int>
         {
             {'F', 0 },
             {'R', 1 },
             {'L', 2},
         };
+
+        private static (int, int) validateGridSize(string y, string x)
+        {
+            int retY = int.TryParse(y, out var numY) ? numY : throw new InvalidOperationException("expected int for y axis of grid");
+            int retX = int.TryParse(x, out var numX) ? numX : throw new InvalidOperationException("expected int for x axis of grid");
+
+            return (retY, retX);
+        }
+
+        private static (int, int) validateStartPos(string y, string x)
+        {
+            int retY = int.TryParse(y, out var numY) ? numY : throw new InvalidOperationException("expected int for y axis start Pos");
+            int retX = int.TryParse(x, out var numX) ? numX : throw new InvalidOperationException("expected int for x axis start Pos");
+
+            return (retY, retX);
+        }
+
+        public struct Current
+        {
+            public string Orientation { get; set; }
+            public (int, int) Postion { get; set; }
+            public (int X, int Y) GridSize { get; set; }
+        }
+
+        private static char[] orientationKeys = new[] { 'N', 'E', 'S', 'W' };
     }
 }
