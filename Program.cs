77d60a85111b3d17b007b4a854796ed7569f6231
Dave Hinton <dave.hinton@qnrl.com>
2019-07-10 21:45:18 +0100

    Initial Commit, input params, initial method ideas

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShipCoordinatesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 6 Args of Your Journey  ");
+
+            string input = Console.ReadLine();
+            string[] ia = input.Split(' ');
+
+            if (ia.Length != 6)
+            {
+                throw new InvalidOperationException("Expected 6 arguments like:\n5 3 1 1 E RFRFRF");
+            }
+
+            var gridSize = (ia[0], ia[1]);
+            var startPos = (ia[2], ia[3]);
+            var startOrientation = ia[4][0];
+            var journey = ia[5];
+
+           Console.WriteLine($"gridSize: {gridSize}, startPos: {startPos}, startOrientation: {startOrientation}, journey: {journey}");
+
+
+        }
+
+       
+
+        private static  (int, int) Orientations(int x, int y)
+        {
+            // returns y & x+ 1 etc
+
+            throw new NotImplementedException();
+
+        }
+
+
+        private static readonly IDictionary<char, int> movements = new Dictionary<char, int>
+        {
+            {'F', 0 },
+            {'R', 1 },
+            {'L', 2},
+        };
+    }
+}
